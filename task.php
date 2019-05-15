d4bd8853434682d78eb863c08de131499fe17901
kushhingol <kushhingol>
2019-05-15 21:37:30 +0530

    deleted the commented code

--- task.php
+++ task.php
@@ -47,27 +47,6 @@ else{
   <nav class="navbar navbar-expand-lg navbar-dark bg-white static-top head">
     <div class="container">
       <a class="navbar-brand" href="#"><img src="logo.png" alt=""></a>
-     <!-- <button class="navbar-toggler nav_button" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation" style="background:black;">
-        <span class="navbar-toggler-icon"></span>
-      </button>
-      <div class="collapse navbar-collapse" id="navbarResponsive">
-        <ul class="navbar-nav ml-auto">
-          <li class="nav-item active">
-            <a class="nav-link" href="#" style="color:black;">Home
-              <span class="sr-only">(current)</span>
-            </a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="#" style="color:black;">About</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="#" style="color:black;">Services</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="#" style="color:black;">Contact</a>
-          </li>
-        </ul>
-      </div>-->
     </div>
   </nav>
 
